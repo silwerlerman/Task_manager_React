465cea512713a3389fcd80fe44bddc3e7179811e
Silweron <silwerlerman.yandex.ru>
2021-05-11 19:43:45 +0300

    Добавлена загрузка данных с сервера в App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
 
+  const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://185.246.66.84:3000/llerman/tasks")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setTasks(result);
+      },
+      (error) => {
+        setError(error);
+      }
+    )
+  })
+
   return (
     <div className="App">
-      
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            {task.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
