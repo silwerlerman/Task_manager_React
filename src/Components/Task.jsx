c7477e5282c3e0cfe28063f9d9f9b691b3a358da
Silweron <silwerlerman.yandex.ru>
2021-05-12 14:05:19 +0300

    Переделка Task с помощью class-names

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,33 +1,19 @@
+import cn from "class-names"
+
 function Task({item}){
     
-    if(!item.completed){
-        return (
-            <div class="task task-todo">
-                <div>
-                    <input type="checkbox"/>
-                    <input type="text" value={item.title} readOnly="true"/>
-                </div>
-                <div>
-                    <button>Редактировать</button>
-                    <button>Удалить</button>
-                </div>
+    return (
+        <div className={cn('task',{'task-completed':item.completed},{'task-todo':!item.completed})}>
+            <div>
+                <input type="checkbox"/>
+                <input type="text" value={item.title} readOnly="true"/>
             </div>
-        );
-    }
-    else{
-        return (
-            <div class="task task-completed">
-                <div>
-                    <input type="checkbox"/>
-                    <input type="text" value={item.title} readOnly="true"/>
-                </div>
-                <div>
-                    <button>Редактировать</button>
-                    <button>Удалить</button>
-                </div>
-            </div> 
-        );
-    }
+            <div>
+                <button>Редактировать</button>
+                <button>Удалить</button>
+            </div>
+        </div>
+    );
     
 }
 
